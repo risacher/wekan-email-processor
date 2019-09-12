b4db44286d67d5c4cf2ec3a03377406f54be7a65
Daniel Risacher <drisacher@gmail.com>
2019-09-12 17:09:05 -0400

    Working, barely.  Does not parse recipient address

--- wekan-api-test.js
+++ wekan-api-test.js
@@ -4,55 +4,68 @@ const fetch = require('node-fetch');
 
 var creds = JSON.parse(fs.readFileSync('creds.json'));
 var test_message = JSON.parse(fs.readFileSync('test-message.json'));
-
+var debug = console.log;
 var url = creds.url;
-var sender = test_message.message.from.value[0].address;
-console.log(test_message);
+var messagesender = test_message.message.from.value[0].address;
+var messagerecip = test_message.message.to.value[0].address;
+var messagesubject = test_message.message.subject;
+var messagetext = test_message.message.text;
 
+debug([messagesender, messagerecip, messagesubject, messagetext]);
+//debug (test_message);
 var headers = {
     "Content-Type": "application/json",
 };
-var status = console.log;
+
 var boards = [];
 var boardCache = {};
+var boardId;
+var swimlaneId;
+var userid;
 
 login().then((rLogin) => {
-  return Promise.all([fetchUsers(),
+  return Promise.all([fetchUsers()
+                      .then((rUsers)=>{ return findUserByEmail(rUsers, messagesender) })
+//                      .then(JSON.stringify)
+                      .then(debug),
                       fetchBoards(rLogin.id)])
-}).then((results) => {
+}).then(results => {
   return fetchBoard(results[1] /*rBoards*/, creds.board);
-}).then((rBoard) => {
-  return rBoard.lists.find((e) => { return e.title == creds.list;  })
-  ._id;
-}).then(lId => { console.log (`THE LIST ID ${lId}`); })
+}).then(rBoard => {
+  //debug(rBoard);
+  boardId = rBoard._id;
+  let defaultswimlane = rBoard.swimlanes.find(e=> { return e.title == 'Default' })
+  if (defaultswimlane.length) { swimlaneId = defaultswimlane[0]._id } 
+  else { swimlaneId = rBoard.swimlanes[0]._id; }
+  return rBoard.lists
+    .find(e => { return e.title == creds.list;  })._id;
+})
+  .then(lId => { return postCard(lId); })
+  .then(debug);
 
 function login() {
   delete headers.Authorization;
-  status('attempting login with username/password');
+  debug('attempting login with username/password');
   return fetch(url + "users/login", {
     method: 'POST',
     headers: headers, 
     body: JSON.stringify({username: creds.username, password: creds.password })
   }).then((response) => { return response.json(); })
     .then((rLogin) => {
-      console.log(rLogin);
+      debug(rLogin);
       let token=rLogin.token;
-      if (token) { status('logged in with username password'); }
-      else { status('username/password login failed'); }
-      
-//      sessionStorage.setItem("wekantoken",token);
+      if (token) { debug('logged in with username/password', token); }
+      else { debug('username/password login failed'); }
       headers.Authorization = "bearer "+rLogin.token;
-      let id=rLogin.id;
-//      sessionStorage.setItem("wekanid",id);
-      console.log(JSON.stringify(rLogin));
-      // document.getElementById('log').innerHTML = JSON.stringify(rLogin);
+      userid=rLogin.id;
+      debug(JSON.stringify(rLogin));
       return rLogin;
     })
 };
 
 
 function fetchBoards(id) {
-  status ('fetching list of boards');
+  debug ('fetching list of boards');
   return fetch(url+"api/users/"+id+"/boards", {
     method: 'GET',
     headers: headers
@@ -60,22 +73,14 @@ function fetchBoards(id) {
     .then((response) => {
       return response.json(); })
     .then((rBoards) => {
-      console.log(rBoards);
+      debug(rBoards);
       // document.rBoards = rBoards;
       if (rBoards.length) {
-        status("found list of "+rBoards.length+" boards available");
-        // var s = document.getElementById('board-select');
-        // s.innerHTML = "";
-        // rBoards.forEach((e,i,a) => {
-        //   let o = document.createElement('option');
-        //   o.setAttribute('value', e._id);
-        //   o.appendChild(document.createTextNode(e.title));
-        //   s.appendChild(o);
-        // });
+        debug("found list of "+rBoards.length+" boards available");
         boards = rBoards;
         return rBoards;
       } else {
-        status("board list not loaded.  Login?");
+        debug("board list not loaded.  Login?");
         return false;
       }
     })
@@ -83,7 +88,7 @@ function fetchBoards(id) {
 
 function fetchBoard(rBoards, board) {
   var bId = rBoards.find((e) => { return e.title == board; })._id; 
-  status (`fetching lists for board ${board} with id ${bId}`);
+  debug (`fetching lists for board ${board} with id ${bId}`);
   
   return fetch(url+"api/boards/"+bId+"/export", {
     method: 'GET',
@@ -92,10 +97,10 @@ function fetchBoard(rBoards, board) {
     // not using .json() to debug Weken returning zero length results
     return response.text(); })
     .then((rBoard) => {
-      //console.log(rBoard.length);
+      //debug(rBoard.length);
       boardCache[bId] = JSON.parse(rBoard);
-      console.log(Object.keys(boardCache[bId]));
-      console.log(boardCache[bId].users);
+      debug(Object.keys(boardCache[bId]));
+      debug(boardCache[bId].users);
       return JSON.parse(rBoard);
     });
 }
@@ -107,18 +112,41 @@ function fetchUsers() {
   }).then((response) => {
     return response.json();
   }).then(rUsers => {
-    console.log('USERS: ', rUsers);
-    Promise.all(
+    debug('USERS: ', rUsers);
+    return Promise.all(
       rUsers.map((e,i,a) => {
         return fetch(url+"api/users/"+e._id, {
           method: 'GET',
           headers: headers
         }).then(resp => { return resp.json(); })
           .then((rUser) => {
-            console.log('user', rUser);
             return rUser;
           })
       }))
   })
 };
-  
+   
+function findUserByEmail(rUsers, addr) {
+  let user = rUsers.find((e1,i,a)=>{
+    return e1.emails.find(e2=>{
+      // FIXME: consider optional test for verified flag
+      return e2.address.toLowerCase() == addr.toLowerCase()
+    })
+  });
+  if (user) { userid = user._id; }
+  return user;
+}
+
+function postCard(lId) {
+  return fetch(url+"api/boards/"+boardId+'/lists/'+lId+'/cards', {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ "title": messagesubject,
+                           "description": messagetext,
+                           "authorId": userid,
+                           "swimlaneId": swimlaneId
+                         })
+  }).then((response) => {
+    return response.text();
+  })
+}
